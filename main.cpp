1e5af096e20b91a40a358f7febbf42e4b23d6bfd
KarolWes <karol.wesolowski01@gmail.com>
2021-12-08 14:53:10 +0100

    untangle, nadal nie działa

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -17,11 +19,29 @@ void print(vector < vector < pair <int, int> > > *braid){
     }
 }
 
-int main() {
-    srand(time(NULL));
-    cout << "Welcome to braid generator" << endl;
-    int n = 3;
-    int h = 10;
+vector < vector < pair <int, int> > > *read_data(string path)
+{
+    fstream f;
+    f.open(path, ios::in);
+    int n, h;
+    f >> n >> h;
+    auto *braid = new vector < vector < pair <int, int> > >;
+    auto *level= new vector < pair <int, int> >;
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < n; j++){
+            int pos, top;
+            f >> pos >> top;
+            level->push_back(make_pair(pos, top));
+        }
+        braid->push_back(*level);
+        level->clear();
+    }
+
+    f.close();
+    return braid;
+}
+
+vector < vector < pair <int, int> > > *generate(int n, int h){
     auto *braid = new vector < vector < pair <int, int> > >;
     auto *level= new vector < pair <int, int> >;
     for(int i = 0; i < n; i++){
@@ -52,6 +72,62 @@ int main() {
         braid->push_back(*level);
         level->clear();
     }
+    return braid;
+}
+
+void untangle(vector < vector < pair <int, int> > > *braid){
+    int knot; //1 if first is on top, -1 if second
+    int test_knot;
+    int start_id;
+    for(int j = 0; j < braid->at(0).size()-1; j++){
+        start_id = 0;
+        knot = 0;
+        int ini_a = braid->at(start_id).at(j).first;
+        int ini_b = braid->at(start_id).at(j+1).first;
+        for(int i = 1; i < braid->size(); i++){
+            int test_a = braid->at(i).at(j).first;
+            int test_b = braid->at(i).at(j+1).first;
+            test_knot = braid->at(i).at(j).second;
+            if (ini_a == test_b && ini_b == test_a) {
+                if (knot == -test_knot) {
+                    for(int m = start_id+1; m< i; m++){
+                        swap(braid->at(m).at(j).first, braid->at(m).at(j+1).first);
+                        braid->at(m).at(j).second = 0;
+                        braid->at(m).at(j+1).second = 0;
+                    }
+                    braid->at(i).at(j).second = 0;
+                    braid->at(i).at(j+1).second = 0;
+                }
+                else{
+                    //test_knot = braid->at(start_id).at(j).second;
+                }
+            }
+            else{
+                if(test_a == ini_a && test_b == ini_b){
+                    test_knot = braid->at(start_id).at(j).second;
+                }
+                else{
+                    start_id = i;
+                }
+            }
+            knot = test_knot;
+            ini_b = test_b;
+            ini_a = test_a;
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    cout << "Welcome to braid generator" << endl;
+    int n = 3;
+    int h = 10;
+    //auto braid = generate(n, h);
+    auto braid = read_data("C:\\Users\\Karol\\CLionProjects\\braids\\test.txt");
+    print(braid);
+    cout << "________\n";
+    untangle(braid);
+    untangle(braid);
     print(braid);
 
     return 0;
